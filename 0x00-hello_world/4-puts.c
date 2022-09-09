46dddf60b274b22c4fbb88a4fef80bebeade5ee1
Caleb Amadi <calebere@yahoo.com>
2022-09-08 21:25:21 -0700

    using put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+#/**
+   * main - Entry point
+   *
+   * Return: always 0
+   * /
+
+int main(void)
+{
+	puts("\"Programing is like multilingual puzzle");
+	return (0)
+
+}	
