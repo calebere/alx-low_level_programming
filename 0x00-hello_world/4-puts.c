6b1f4a3308910158f3f60b028b718da7fc88c1b8
Caleb Amadi <calebere@yahoo.com>
2022-09-08 21:52:20 -0700

    using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-   * main - Entry point
-   *
-   * Return: always 0
+	* main - Entry point
+	*
+	* Return: always 0
 
 */
 
@@ -11,4 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
-}	
+}
