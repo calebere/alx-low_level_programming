13c0ca9e25ac02b6b19f8e1f41e511231d9da5b7
Caleb Amadi <calebere@yahoo.com>
2022-09-15 11:00:30 -0700

    print alphabet in lower case then followed with uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 
 	char a;
-		for (a = a; a <= z ; a++)
+		for (a = 97; a <= 123 ; a++)
 		putchar(a);
-		for (a = A; a <= Z; a++)
+		for (a = 65; a <= 91; a++)
 		putchar(a);
 	putchar(n);
 return (0);
