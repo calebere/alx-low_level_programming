d61d9aa36d7780792e9d7e64cb0d5a822df68617
Caleb Amadi <calebere@yahoo.com>
2022-09-19 18:59:00 -0700

    main header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
-
+#include "main.h"
 /**
  * _putchar - write the character c
  * @c: the character to print
  * Return: on seccess 1
- * On error, -1 is returned and error is set properly
 */
 
 int _putchar(char c)
