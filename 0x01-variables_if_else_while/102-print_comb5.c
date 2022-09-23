5a176a9d22706e9f82eaa1757a9af144cb3b761d
Caleb Amadi <calebere@yahoo.com>
2022-09-23 13:46:06 -0700

    print all combination of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,41 +9,25 @@
 int main(void)
 
 {
-	int i, e, g, h, op1, op2;
+	int i;
+	int j;
 
-	i = e = g = h = 48;
-	while (h < 58)
+	for (i = 0; i <= 99; i++)
 	{
-		g = 48;
-		while (g < 58)
+		for (j = i + 1; j <= 99; j++)
 		{
-			e = 48;
-			while (e < 58)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
 			{
-				i = 48;
-				while (i < 58)
-				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if ((h == 57) && (g == 56) && (e == 57) && (i == 57))
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
-				}
-				e++;
+				putchar(',');
+				putchar(' ');
 			}
-			g++;
 		}
-		h++;
 	}
 	putchar('\n');
 	return (0);
