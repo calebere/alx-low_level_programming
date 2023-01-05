d2adf498e24161fed6ed3b0675586cfd727be578
calebere <calebere@yahoo.com>
2023-01-05 14:08:43 -0800

    main c file

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -5,9 +5,9 @@
 
 /**
  * main - check the code for
- *
  * Return: Always EXIT_SUCCESS.
  */
+
 int main(void)
 {
     hash_table_t *ht;
@@ -15,3 +15,4 @@ int main(void)
     ht = hash_table_create(1024);
     printf("%p\n", (void *)ht);
     return (EXIT_SUCCESS);
+}
